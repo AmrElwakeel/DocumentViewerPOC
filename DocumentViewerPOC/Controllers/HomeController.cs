9a1cfa09bc083963b45cc4304d49f8e9392028a7
Amr Elwakeel <amrelwakeel96@hotmail.com>
2022-07-18 14:50:34 +0200

    Add project files.

---
+++ DocumentViewerPOC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using DocumentViewerPOC.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentViewerPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment environment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment _environment)
+        {
+            _logger = logger;
+            environment = _environment;
+        }
+
+        public IActionResult Index()
+        {
+            //var filePath = Path.Combine(this.environment.WebRootPath, "Documents\\TassniefWF.pdf");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
